008e747fb8067edb5bec4ce80c1c2eda5fc71dad
Julian Berman <Julian+git@GrayVines.com>
2011-12-30 14:03:11 -0500

    setup.py and tox

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name="jsonschema",
+    version = "0.1dev",
+    author = "Julian Berman",
+    author_email = "Julian@GrayVines.com",
+    description = "An implementation of JSON-Schema validation for Python",
+    license = "MIT/X",
+    url = "http://github.com/Julian/jsonschema",
+)
