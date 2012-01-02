1f0e66bf614fda68d0809b9fd7fae8e240cff618
Julian Berman <Julian+git@GrayVines.com>
2012-01-01 21:42:40 -0500

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from distutils.core import setup
 
+from jsonschema import __version__
+
 
 setup(
     name="jsonschema",
-    version = "0.1dev",
-    author = "Julian Berman",
-    author_email = "Julian@GrayVines.com",
-    description = "An implementation of JSON-Schema validation for Python",
-    license = "MIT/X",
-    url = "http://github.com/Julian/jsonschema",
+    version=__version__,
+    py_modules=["jsonschema"],
+    author="Julian Berman",
+    author_email="Julian@GrayVines.com",
+    description="An implementation of JSON-Schema validation for Python",
+    license="MIT/X",
+    url="http://github.com/Julian/jsonschema",
 )
