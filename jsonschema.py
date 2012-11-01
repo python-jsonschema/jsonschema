5618b4526f85fb8e04f9938cd052035e9aa6742b
Julian Berman <Julian+git@GrayVines.com>
2012-11-01 09:35:56 -0400

    Fix a directive

--- jsonschema.py
+++ jsonschema.py
@@ -4,8 +4,8 @@ An implementation of JSON Schema for Python
 The main functionality is provided by the validator classes for each of the
 supported JSON Schema versions.
 
-Most commonly, the :function:`validate` function is the quickest way to simply
-validate a given instance under a schema, and will create a validator for you.
+Most commonly, :func:`validate` is the quickest way to simply validate a given
+instance under a schema, and will create a validator for you.
 
 """
 
