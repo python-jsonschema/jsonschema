1b1f747c368207c3f1f93d1f783dc7cbe3ab5de3
Julian Berman <Julian@GrayVines.com>
2013-01-12 23:15:22 -0500

    Sphinx doctest

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,12 @@ sys.path.insert(0, os.path.abspath(os.path.pardir))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.coverage', 'sphinx.ext.viewcode']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.coverage',
+    'sphinx.ext.viewcode',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -32,7 +37,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'jsonschema'
-copyright = u'2012, Julian Berman'
+copyright = u'2013, Julian Berman'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -76,6 +81,8 @@ pygments_style = 'sphinx'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
+
+doctest_global_setup = "from jsonschema import *"
 
 
 # -- Options for HTML output ---------------------------------------------------
