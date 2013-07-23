b7289fe17815b7d10dd15c8c142ebeca23921891
AndrewSC <asc9003@rit.edu>
2013-07-23 10:44:46 -0500

    fixing message

--- jsonschema/_validators.py
+++ jsonschema/_validators.py
@@ -306,7 +306,7 @@ def maxProperties_draft4(validator, mP, instance, schema):
     if not validator.is_type(instance, "object"):
         return
     if validator.is_type(instance, "object") and len(instance) > mP:
-        yield ValidationError("%r is too short" % (instance,))
+        yield ValidationError("%r is too long" % (instance,))
 
 
 def allOf_draft4(validator, allOf, instance, schema):
