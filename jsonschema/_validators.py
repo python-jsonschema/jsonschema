5670b184af7f738e0811183bbfc9726b2d74d8fb
Julian Berman <Julian@GrayVines.com>
2013-07-28 09:56:25 -0400

    Line length.

--- jsonschema/_validators.py
+++ jsonschema/_validators.py
@@ -299,7 +299,9 @@ def required_draft4(validator, required, instance, schema):
 
 def minProperties_draft4(validator, mP, instance, schema):
     if validator.is_type(instance, "object") and len(instance) < mP:
-        yield ValidationError("%r does not have enough properties" % (instance,))
+        yield ValidationError(
+            "%r does not have enough properties" % (instance,)
+        )
 
 
 def maxProperties_draft4(validator, mP, instance, schema):
