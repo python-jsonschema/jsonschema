00e05898bc2c167db8c0f99e33fb8e4449b2ea5c
Julian Berman <Julian@GrayVines.com>
2013-12-08 14:08:27 -0500

    Update classifiers.

--- setup.py
+++ setup.py
@@ -20,8 +20,6 @@ classifiers = [
     "Programming Language :: Python :: 2.6",
     "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.1",
-    "Programming Language :: Python :: 3.2",
     "Programming Language :: Python :: 3.3",
     "Programming Language :: Python :: Implementation :: CPython",
     "Programming Language :: Python :: Implementation :: PyPy",
