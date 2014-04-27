b576c08e4c7059110d16a126cbdbd9ac3102f525
Julian Berman <Julian@GrayVines.com>
2014-04-27 15:35:58 -0400

    Point at 2.7

--- docs/conf.py
+++ docs/conf.py
@@ -78,7 +78,7 @@ doctest_global_setup = dedent("""
     from jsonschema import *
 """)
 
-intersphinx_mapping = {"python": ("http://docs.python.org/3.2", None)}
+intersphinx_mapping = {"python": ("http://docs.python.org/2.7", None)}
 
 
 # -- Options for HTML output ---------------------------------------------------
