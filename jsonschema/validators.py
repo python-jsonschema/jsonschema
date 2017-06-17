307313bce1f9d4e57c5e4bdfa39ab5c5fad3d41b
Julian Berman <Julian@GrayVines.com>
2017-06-17 13:21:40 -0400

    patternProperties

--- jsonschema/validators.py
+++ jsonschema/validators.py
@@ -271,6 +271,7 @@ Draft6Validator = create(
         u"multipleOf": _validators.multipleOf,
         u"not": _validators.not_,
         u"oneOf": _validators.oneOf_draft6,
+        u"patternProperties": _validators.patternProperties,
         u"properties": _validators.properties,
         u"required": _validators.required,
         u"type": _validators.type,
