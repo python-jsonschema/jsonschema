0252d77fce24e14e38012e386cede7f2cdceadee
Julian Berman <Julian@GrayVines.com>
2017-10-19 20:06:25 +0200

    And now make both the test running and the perf running use the shared loader.

--- benchmarks/test_suite.py
+++ benchmarks/test_suite.py
@@ -1,30 +1,34 @@
-#!/usr/env/bin python
-"""Benchmark the performance of jsonschema.
+#!/usr/bin/env python
+"""
+A performance benchmark using the official test suite.
 
 This benchmarks jsonschema using every valid example in the
 JSON-Schema-Test-Suite. It will take some time to complete.
 """
-
-import os
-
 from perf import Runner
-from utils import load_json_test_cases, performance_test
 
-import jsonschema
+from jsonschema import ValidationError
+from jsonschema.tests._suite import Suite
 
 
-REPO_ROOT = os.path.join(os.path.dirname(jsonschema.__file__), os.path.pardir)
-SUITE = os.getenv("JSON_SCHEMA_TEST_SUITE", os.path.join(REPO_ROOT, "json"))
-TESTS_DIR = os.path.join(SUITE, "tests")
+def swallow_errors(test):
+    def func():
+        try:
+            test.validate()
+        except ValidationError:
+            pass
+    return func
 
 
 def main():
     runner = Runner()
-    draft3_cases = load_json_test_cases(os.path.join(TESTS_DIR, 'draft3'), 'draft3')
-    performance_test(runner, draft3_cases, jsonschema.Draft3Validator)
-    draft4_cases = load_json_test_cases(os.path.join(TESTS_DIR, 'draft4'), 'draft4')
-    performance_test(runner, draft4_cases, jsonschema.Draft4Validator)
-
+    suite = Suite()
+    for version in suite.versions():
+        for test in version.tests():
+            runner.bench_func(
+                name=test.fully_qualified_name,
+                func=swallow_errors(test),
+            )
 
 
 if __name__ == "__main__":
