f781501cfecd511992d96ea05337f6f648d32448
Julian Berman <Julian@GrayVines.com>
2018-08-26 08:29:28 +0100

    A pesky one, and this test isn't great.
    But unblock us. We can think about cleverer
    things later.

--- jsonschema/tests/test_exceptions.py
+++ jsonschema/tests/test_exceptions.py
@@ -3,7 +3,6 @@ import textwrap
 
 from jsonschema import Draft4Validator, exceptions
 from jsonschema.compat import PY3
-from jsonschema.tests.compat import mock
 
 
 class TestBestMatch(TestCase):
@@ -381,9 +380,61 @@ class TestErrorInitReprStr(TestCase):
         )
 
     def test_uses_pprint(self):
-        with mock.patch("pprint.pformat") as pformat:
-            str(self.make_error())
-            self.assertEqual(pformat.call_count, 2)  # schema + instance
+        self.assertShows(
+            """
+            Failed validating u'maxLength' in schema:
+                {0: 0,
+                 1: 1,
+                 2: 2,
+                 3: 3,
+                 4: 4,
+                 5: 5,
+                 6: 6,
+                 7: 7,
+                 8: 8,
+                 9: 9,
+                 10: 10,
+                 11: 11,
+                 12: 12,
+                 13: 13,
+                 14: 14,
+                 15: 15,
+                 16: 16,
+                 17: 17,
+                 18: 18,
+                 19: 19}
+
+            On instance:
+                [0,
+                 1,
+                 2,
+                 3,
+                 4,
+                 5,
+                 6,
+                 7,
+                 8,
+                 9,
+                 10,
+                 11,
+                 12,
+                 13,
+                 14,
+                 15,
+                 16,
+                 17,
+                 18,
+                 19,
+                 20,
+                 21,
+                 22,
+                 23,
+                 24]
+            """,
+            instance=list(range(25)),
+            schema=dict(zip(range(20), range(20))),
+            validator=u"maxLength",
+        )
 
     def test_str_works_with_instances_having_overriden_eq_operator(self):
         """
