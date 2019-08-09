97c43b3c9ba5ffcecff79490c0a1567b3a441058
Igor Savchuk <trollfred@gmail.com>
2019-08-09 14:59:14 +0200

    Handle DistributionNotFound

--- jsonschema/__init__.py
+++ jsonschema/__init__.py
@@ -29,5 +29,8 @@ from jsonschema.validators import (
     validate,
 )
 
-from pkg_resources import get_distribution
-__version__ = get_distribution(__name__).version
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    __version__ = None
