10f8a3e525f63f10555aeccd06e5d978e8a6958f
Nicolas Aimetti <naimetti@yahoo.com.ar>
2019-10-28 16:57:50 -0300

    Add format validators as separate modules

--- jsonschema/_format.py
+++ jsonschema/_format.py
@@ -248,23 +248,27 @@ else:
 try:
     import rfc3987
 except ImportError:
-    from jsonschema._format_validators import validate_rfc3986
 
-    @_checks_drafts(name="uri",)
-    def is_uri(instance):
-        if not isinstance(instance, str_types):
-            return True
-        return validate_rfc3986(instance, rule="URI")
+    try:
+        from rfc3986_validator import validate_rfc3986
+    except ImportError:
+        pass
+    else:
+        @_checks_drafts(name="uri",)
+        def is_uri(instance):
+            if not isinstance(instance, str_types):
+                return True
+            return validate_rfc3986(instance, rule="URI")
 
-    @_checks_drafts(
-        draft6="uri-reference",
-        draft7="uri-reference",
-        raises=ValueError,
-    )
-    def is_uri_reference(instance):
-        if not isinstance(instance, str_types):
-            return True
-        return validate_rfc3986(instance, rule="URI_reference")
+        @_checks_drafts(
+            draft6="uri-reference",
+            draft7="uri-reference",
+            raises=ValueError,
+        )
+        def is_uri_reference(instance):
+            if not isinstance(instance, str_types):
+                return True
+            return validate_rfc3986(instance, rule="URI_reference")
 
 else:
     @_checks_drafts(draft7="iri", raises=ValueError)
@@ -299,21 +303,23 @@ else:
 try:
     from strict_rfc3339 import validate_rfc3339
 except ImportError:
-    from jsonschema._format_validators import validate_rfc3339
+    try:
+        from rfc3339_validator import validate_rfc3339
+    except ImportError:
+        validate_rfc3339 = None
 
+if validate_rfc3339:
+    @_checks_drafts(name="date-time")
+    def is_datetime(instance):
+        if not isinstance(instance, str_types):
+            return True
+        return validate_rfc3339(instance)
 
-@_checks_drafts(name="date-time")
-def is_datetime(instance):
-    if not isinstance(instance, str_types):
-        return True
-    return validate_rfc3339(instance)
-
-
-@_checks_drafts(draft7="time")
-def is_time(instance):
-    if not isinstance(instance, str_types):
-        return True
-    return is_datetime("1970-01-01T" + instance)
+    @_checks_drafts(draft7="time")
+    def is_time(instance):
+        if not isinstance(instance, str_types):
+            return True
+        return is_datetime("1970-01-01T" + instance)
 
 
 @_checks_drafts(name="regex", raises=re.error)
