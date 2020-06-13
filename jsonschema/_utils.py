c6f3cf79bb4d0e34db0cdaa080aa95d20af6e634
Ryan Miller <rjmill@cumulusnetworks.com>
2020-06-13 19:28:18 -0400

    Remove dead code in `uniq`
    Now that `unbool` creates a hashable, the fallback options for `uniq`
    are unnecessary.

--- jsonschema/_utils.py
+++ jsonschema/_utils.py
@@ -1,4 +1,3 @@
-import itertools
 import json
 import pkgutil
 import re
@@ -198,26 +197,6 @@ def unbool(element, true=object(), false=object()):
 def uniq(container):
     """
     Check if all of a container's elements are unique.
-
-    Successively tries first to rely that the elements are hashable, then
-    falls back on them being sortable, and finally falls back on brute
-    force.
     """
 
-    try:
-        return len(set(unbool(i) for i in container)) == len(container)
-    except TypeError:
-        try:
-            sort = sorted(unbool(i) for i in container)
-            sliced = itertools.islice(sort, 1, None)
-            for i, j in zip(sort, sliced):
-                if i == j:
-                    return False
-        except (NotImplementedError, TypeError):
-            seen = []
-            for e in container:
-                e = unbool(e)
-                if e in seen:
-                    return False
-                seen.append(e)
-    return True
+    return len(set(unbool(i) for i in container)) == len(container)
