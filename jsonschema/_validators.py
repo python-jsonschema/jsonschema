24f4f41e129c736eaf1cf0e2bb00c88ceb854ce7
Will Flatau <William.Flatau@coxautoinc.com>
2021-04-11 01:52:47 -0400

    Removed unnecessary line.

--- jsonschema/_validators.py
+++ jsonschema/_validators.py
@@ -331,14 +331,12 @@ def allOf(validator, allOf, instance, schema):
         if u"properties" in subschema:
             all_properties.update(subschema["properties"])
         elif u"$ref" in subschema:
-            resolve = getattr(validator.resolver, "resolve", None)
             scope, resolved = validator.resolver.resolve(subschema[u"$ref"])
             if u"properties" in resolved:
                 all_properties.update(resolved[u"properties"])
     for index, subschema in enumerate(allOf):
         if hasattr(subschema, '__iter__'):
             subschema["properties"] = all_properties
-
         for error in validator.descend(instance, subschema, schema_path=index):
             yield error
 
