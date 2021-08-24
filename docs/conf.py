2a9bb096ba5ede6a055c3d2dff35345aa59cbf91
Julian Berman <Julian@GrayVines.com>
2021-08-24 13:07:14 +0100

    Be furoious.

--- docs/conf.py
+++ docs/conf.py
@@ -89,11 +89,12 @@ intersphinx_mapping = {
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "pyramid"
+html_theme = "furo"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
+#
 # html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
