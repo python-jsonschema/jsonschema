689f3cd09b8b432f2458e711fe4a678182f9403c
PeterHolzner <58387550+GenosW@users.noreply.github.com>
2022-05-20 11:45:43 +0200

    Update _validators.py

--- jsonschema/_validators.py
+++ jsonschema/_validators.py
@@ -186,7 +186,7 @@ def multipleOf(validator, dB, instance, schema):
     if isinstance(dB, float):
         quotient = instance / dB
         try:
-            failed = math.isclose(round(quotient), quotient)
+            failed = isclose(round(quotient), quotient)
         except OverflowError:
             # When `instance` is large and `dB` is less than one,
             # quotient can overflow to infinity; and then casting to int
