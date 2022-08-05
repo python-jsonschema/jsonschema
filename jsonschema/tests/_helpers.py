86a5fff9add5980892c90a3e34476802dec7a6dc
Julian Berman <Julian@GrayVines.com>
2022-08-05 11:30:37 +0300

    Improve the internal skipped-test helper messages.

--- jsonschema/tests/_helpers.py
+++ jsonschema/tests/_helpers.py
@@ -1,5 +1,29 @@
+from urllib.parse import urljoin
+
+
+def issues_url(organization, repository):
+    return urljoin(
+        urljoin(
+            urljoin("https://github.com", organization),
+            repository,
+        ),
+        "issues",
+    )
+
+
+ISSUES_URL = issues_url("python-jsonschema", "jsonschema")
+TEST_SUITE_ISSUES_URL = issues_url("json-schema-org", "JSON-Schema-Test-Suite")
+
+
 def bug(issue=None):
     message = "A known bug."
     if issue is not None:
-        message += " See issue #{issue}.".format(issue=issue)
+        message += f" See {urljoin(ISSUES_URL, str(issue))}."
     return message
+
+
+def test_suite_bug(issue):
+    return (
+        "A known test suite bug. "
+        f"See {urljoin(TEST_SUITE_ISSUES_URL, str(issue))}."
+    )
