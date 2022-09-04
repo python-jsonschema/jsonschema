05b18609db84954d7309d69d7628cd882ba90dc3
Julian Berman <Julian@GrayVines.com>
2022-09-04 12:40:34 +0300

    Remove some now-unnecessary path mangling in doc builds.

--- docs/conf.py
+++ docs/conf.py
@@ -1,22 +1,29 @@
-from importlib import metadata
 from pathlib import Path
-import os
+import importlib.metadata
 import re
-import sys
 
 ROOT = Path(__file__).parent.parent
 PACKAGE_SRC = ROOT / "jsonschema"
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-ext_paths = [os.path.abspath(os.path.pardir), os.path.dirname(__file__)]
-sys.path = ext_paths + sys.path
+# -- Project information -----------------------------------------------------
 
-# -- General configuration -------------------------------------------------
+project = "jsonschema"
+author = "Julian Berman"
+copyright = "2013, " + author
+
+# version: The short X.Y version
+# release: The full version, including alpha/beta/rc tags.
+release = importlib.metadata.version("jsonschema")
+version = release.partition("-")[0]
+
+
+# -- General configuration ---------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
+#
 # needs_sphinx = "1.0"
+
+default_role = "any"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named "sphinx.ext.*") or your custom
@@ -43,28 +50,14 @@ cache_path = "_cache"
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
-# The suffix of source filenames.
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = [".rst", ".md"]
 source_suffix = ".rst"
-
-# The encoding of source files.
-# source_encoding = "utf-8-sig"
 
 # The master toctree document.
 master_doc = "index"
-
-# General information about the project.
-project = "jsonschema"
-author = "Julian Berman"
-copyright = "2013, " + author
-
-# The version info for the project you're documenting, acts as replacement for
-# |version| and |release|, also used in various other places throughout the
-# built documents.
-#
-# version: The short X.Y version
-# release: The full version, including alpha/beta/rc tags.
-release = metadata.version("jsonschema")
-version = release.partition("-")[0]
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
@@ -74,17 +67,12 @@ version = release.partition("-")[0]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "_cache", "_static", "_templates"]
-
-# The reST default role (used for this markup: `text`) to use for all documents
-default_role = "any"
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = "lovelace"
 pygments_dark_style = "one-dark"
-
-# A list of ignored prefixes for module index sorting.
-# modindex_common_prefix = []
 
 doctest_global_setup = """
 from jsonschema import *
@@ -108,115 +96,33 @@ html_theme = "furo"
 #
 # html_theme_options = {}
 
-# Add any paths that contain custom themes here, relative to this directory.
-# html_theme_path = []
-
-# The name for this set of Sphinx documents.  If None, it defaults to
-# "<project> v<release> documentation".
-# html_title = None
-
-# A shorter title for the navigation bar.  Default is the same as html_title.
-# html_short_title = None
-
-# The name of an image file (relative to this directory) to place at the top
-# of the sidebar.
-# html_logo = None
-
-# The name of an image file (within the static path) to use as favicon of the
-# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
-# pixels large.
-# html_favicon = None
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ["_static"]
 
-# If not "", a "Last updated on:" timestamp is inserted at every page bottom,
-# using the given strftime format.
-# html_last_updated_fmt = "%b %d, %Y"
 
-# If true, SmartyPants will be used to convert quotes and dashes to
-# typographically correct entities.
-# html_use_smartypants = True
-
-# Custom sidebar templates, maps document names to template names.
-# html_sidebars = {}
-
-# Additional templates that should be rendered to pages, maps page names to
-# template names.
-# html_additional_pages = {}
-
-# If false, no module index is generated.
-# html_domain_indices = True
-
-# If false, no index is generated.
-# html_use_index = True
-
-# If true, the index is split into individual pages for each letter.
-# html_split_index = False
-
-# If true, links to the reST sources are added to the pages.
-# html_show_sourcelink = True
-
-# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-# html_show_sphinx = True
-
-# If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-# html_show_copyright = True
-
-# If true, an OpenSearch description file will be output, and all pages will
-# contain a <link> tag referring to it.  The value of this option must be the
-# base URL from which the finished HTML is served.
-# html_use_opensearch = ""
-
-# This is the file name suffix for HTML files (e.g. ".xhtml").
-# html_file_suffix = None
+# -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = "jsonschemadoc"
 
 
-# -- Options for LaTeX output ----------------------------------------------
+# -- Options for LaTeX output ------------------------------------------------
 
-# Grouping the document tree into LaTeX files. List of tuples (source
-# start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
     ("index", "jsonschema.tex", "jsonschema Documentation", author, "manual"),
 ]
 
-# The name of an image file (relative to this directory) to place at the top of
-# the title page.
-# latex_logo = None
 
-# For "manual" documents, if this is true, then toplevel headings are parts,
-# not chapters.
-# latex_use_parts = False
-
-# If true, show page references after internal links.
-# latex_show_pagerefs = False
-
-# If true, show URL addresses after external links.
-# latex_show_urls = False
-
-# Documents to append as an appendix to all manuals.
-# latex_appendices = []
-
-# If false, no module index is generated.
-# latex_domain_indices = True
-
-
-# -- Options for manual page output ----------------------------------------
+# -- Options for manual page output ------------------------------------------
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [("index", "jsonschema", "jsonschema Documentation", [author], 1)]
 
-# If true, show URL addresses after external links.
-# man_show_urls = False
 
-
-# -- Options for Texinfo output --------------------------------------------
+# -- Options for Texinfo output ----------------------------------------------
 
 # Grouping the document tree into Texinfo files. List of tuples
 # (source start file, target name, title, author,
@@ -233,16 +139,7 @@ texinfo_documents = [
     ),
 ]
 
-# Documents to append as an appendix to all manuals.
-# texinfo_appendices = []
-
-# If false, no module index is generated.
-# texinfo_domain_indices = True
-
-# How to display URL addresses: "footnote", "no", or "inline".
-# texinfo_show_urls = "footnote"
-
-# -- Options for the linkcheck builder ------------------------------------
+# -- Options for the linkcheck builder --------------------------------------
 
 
 def entire_domain(host):
@@ -263,7 +160,7 @@ autosectionlabel_prefix_document = True
 
 spelling_word_list_filename = "spelling-wordlist.txt"
 
-# -- Options for autoapi --------------------------------------------------
+# -- Options for autoapi ----------------------------------------------------
 
 suppress_warnings = [
     "autoapi.python_import_resolution",
