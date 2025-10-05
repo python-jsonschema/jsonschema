c3855b9bc9cc7264c3ea4d0eb056720f48eac654
Guðni Már Gilbert <gudni.m.g@gmail.com>
2025-10-05 17:24:24 +0000

    Fix a style issue raised in CI
    RUF059 Unpacked variable `error` is never used

--- jsonschema/tests/test_exceptions.py
+++ jsonschema/tests/test_exceptions.py
@@ -321,7 +321,7 @@ class TestBestMatch(TestCase):
 
     def test_one_error(self):
         validator = _LATEST_VERSION({"minProperties": 2})
-        error, = validator.iter_errors({})
+        validator.iter_errors({})
         self.assertEqual(
             exceptions.best_match(validator.iter_errors({})).validator,
             "minProperties",
